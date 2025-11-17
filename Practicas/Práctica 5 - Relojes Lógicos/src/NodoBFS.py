e56f44cb4deb3fa9709a79983e6428d466827169
Zerabalus <hildajoanacg@ciencias.unam.mx>
2025-11-17 03:32:19 -0600

    Add || BFS

--- Practicas/Práctica 5 - Relojes Lógicos/src/NodoBFS.py
+++ Practicas/Práctica 5 - Relojes Lógicos/src/NodoBFS.py
@@ -13,18 +13,21 @@ class NodoBFS(Nodo):
         ''' Aquí va tu implementacion '''
         super().__init__(id_nodo,vecinos,canal_entrada,canal_salida)
         self.padre = id_nodo
-        self.distancia = math.inf
+        self.distancia = math.inf #Inicializa distancia
 
     def bfs(self,env):
-        if self.id_nodo == 0:
+        if self.id_nodo == 0: #el nodo distinguido
             yield env.timeout(TICK)
-            self.distancia = 0
-            self.canal_salida.envia((self.id_nodo,0), self.vecinos)
+            self.distancia = 0 # level_i ← 0
+            self.canal_salida.envia((self.id_nodo,0), self.vecinos)  # Envía a todos los vecinos
 
-        while True:
+        while True: # Recibe el msj
             (padre,distancia) = yield self.canal_entrada.get()
-            if distancia + 1 < self.distancia:
+            if distancia + 1 < self.distancia: # Calcula distancia mínima
+                # Actualiza la distancia y el padre
                 self.distancia = distancia + 1
                 self.padre = padre
+                
                 yield env.timeout(TICK)
+                # Envía el mensaje a todos los vecinos
                 self.canal_salida.envia((self.id_nodo,self.distancia),self.vecinos)
